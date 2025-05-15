450a28fcebb70c66fe25178485ce5e652250c534
APRENDIZ <APRENDIZ@BOGDAFCGFFSDD706.FormacionSena.RED>
2025-05-15 09:51:57 -0500

    app luna

--- App.tsx
+++ App.tsx
@@ -1,131 +1,58 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import React from "react";
+import { View,Text, StyleSheet, Image, TextInputComponent, TextInput, TouchableOpacity } from "react-native";
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+const estilos=StyleSheet.create({
+contenedor:{
+  width:'100%',
+  padding:20,
+  backgroundColor: '#4f3e80',
+  height: '100%',
+  marginTop:0 
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
+},
+imagen:{
+width:'100%',
+  padding:20,
+  backgroundColor: '#4f3e80',
+  height: '50%',
+  marginTop:10,
+  borderRadius: 15 
+},
+texto:{
+  fontSize:50,
+  color: 'black'
+},
+Boton:{
+  fontFamily:'Comic Sans MS',
+  borderColor:'black',
+  borderWidth:1.2,
+  borderRadius:10,
+  marginTop:15,
+  width:45,
+  height: 20,
+  backgroundColor:'#ffffff'
+},
+Input:{
+  borderColor:'black',
+  borderWidth:1.5,
+  borderRadius:15,
+  marginTop:20
 }
 
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  /*
-   * To keep the template simple and small we're adding padding to prevent view
-   * from rendering under the System UI.
-   * For bigger apps the recommendation is to use `react-native-safe-area-context`:
-   * https://github.com/AppAndFlow/react-native-safe-area-context
-   *
-   * You can read more about it here:
-   * https://github.com/react-native-community/discussions-and-proposals/discussions/827
-   */
-  const safePadding = '5%';
-
-  return (
-    <View style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        style={backgroundStyle}>
-        <View style={{paddingRight: safePadding}}>
-          <Header/>
-        </View>
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-            paddingHorizontal: safePadding,
-            paddingBottom: safePadding,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
+})
+export default function App(){
+  return(
+  <View style={estilos.contenedor}>
+    <Text style={estilos.texto}>Buenos días</Text>
+    <View>
+     <TextInput placeholder="Escriba" style={estilos.Input}/>
+     <TouchableOpacity style={estilos.Boton}>
+      <Text>
+        Aqui
+      </Text>
+     </TouchableOpacity>
     </View>
-  );
+   <Image source={require('./img/hola.jpg')} style={estilos.imagen}/>
+  </View>
+)
 }
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
-
-export default App;
